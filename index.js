822c9142c52c1a74b9e059b1f0f0c3718c8db793
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:31:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ class BoardMember {
     this.homeState = homeState
     this.training = training
   }
-  veto { return 'No, I must disagree'}
-  approve { return 'You can do that!'}
-  doCharity { return 'I like to help people.'}
-  releasePressStatement { return 'You will see great things from Scuber.'}
+  veto() { return 'No, I must disagree'}
+  approve() { return 'You can do that!'}
+  doCharity() { return 'I like to help people.'}
+  releasePressStatement() { return 'You will see great things from Scuber.'}
 
 }
