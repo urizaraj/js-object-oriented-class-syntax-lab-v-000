91144ea414c73b366706896904c90dbfff2fe266
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 19:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ class BoardMember {
   approve() { return 'You can do that!'}
   doCharity() { return 'I like to help people.'}
   releasePressStatement() { return 'You will see great things from Scuber.'}
-
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
 }
